fb1db03c645c983916d2787e52bcf9a8bfc2b996
raholm <rasmus.holm723@gmail.com>
2017-11-09 15:40:31 +0100

    Final solution to week1 of dsaas.

--- data_structures_and_algorithms_specialization/algoritmic_toolbox/week01/max_pairwise_product.cpp
+++ data_structures_and_algorithms_specialization/algoritmic_toolbox/week01/max_pairwise_product.cpp
@@ -5,14 +5,14 @@ using std::vector;
 using std::cin;
 using std::cout;
 
-int max_pairwise_product_slow(const vector<int>& numbers) {
-  int result = 0;
-  int n = numbers.size();
-  int prod;
+unsigned long long max_pairwise_product_slow(const vector<unsigned>& numbers) {
+  unsigned long long result = 0;
+  unsigned n = numbers.size();
+  unsigned long long prod;
 
-  for (int i = 0; i < n; ++i) {
-    for (int j = i + 1; j < n; ++j) {
-      prod = numbers.at(i) * numbers.at(j);
+  for (unsigned i = 0; i < n; ++i) {
+    for (unsigned j = i + 1; j < n; ++j) {
+      prod = (unsigned long long) numbers.at(i) * numbers.at(j);
 
       if (prod > result) {
         result = prod;
@@ -23,17 +23,35 @@ int max_pairwise_product_slow(const vector<int>& numbers) {
   return result;
 }
 
+unsigned long long max_pairwise_product_fast(const vector<unsigned>& numbers) {
+  unsigned n = numbers.size();
+
+  int max_index1 = 0, max_index2 = -1;
+
+  for (unsigned i = 1; i < n; ++i) {
+    if (numbers.at(i) > numbers.at(max_index1))
+      max_index1 = i;
+  }
+
+  for (unsigned i = 0; i < n; ++i) {
+    if (i != max_index1 && (max_index2 == -1 || numbers.at(i) > numbers.at(max_index2)))
+      max_index2 = i;
+  }
+
+  return (unsigned long long) numbers.at(max_index1) * numbers.at(max_index2);
+}
+
 int main() {
   int n;
   cin >> n;
 
-  vector<int> numbers(n);
+  vector<unsigned> numbers(n);
 
-  for (int i = 0; i < n; ++i) {
+  for (unsigned i = 0; i < n; ++i) {
     cin >> numbers.at(i);
   }
 
-  int result = max_pairwise_product_slow(numbers);
+  unsigned long long result = max_pairwise_product_fast(numbers);
   cout << result << "\n";
   return 0;
 }
